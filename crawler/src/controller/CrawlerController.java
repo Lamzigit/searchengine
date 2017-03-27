8be99f93df5ab9bbde3b755ba446dc4c38ad6c15
林志杰 <18814103637@163.com>
2017-03-27 23:04:17 +0800

    爬虫控制器 一个控制器类处理一个网站，内部使用了Crawler对象

---
+++ crawler/src/controller/CrawlerController.java
@@ -0,0 +1,75 @@
+package controller;
+
+import cralwer.Crawler;
+import extractor.LinkExtractor;
+import extractor.TextExtractor;
+import frontier.TodoFrontier;
+import frontier.VistedFrontier;
+import producer.UrlProducer;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by 林志杰 on 2017/3/26.
+ */
+public class CrawlerController {
+    public int threadnum = 0;
+
+    public String getPath() {
+        return path;
+    }
+
+    private VistedFrontier vistedFrontier = new VistedFrontier("visitedDB");
+
+    private TodoFrontier todoFrontier = new TodoFrontier();
+
+    public LinkExtractor getLinkExtractor() {
+        return linkExtractor;
+    }
+
+    public TextExtractor getTextExtractor() {
+        return textExtractor;
+    }
+
+    public VistedFrontier getVistedFrontier() {
+        return vistedFrontier;
+    }
+
+    public TodoFrontier getTodoFrontier() {
+        return todoFrontier;
+    }
+
+    //文件存储路径
+    private String path = "";
+    //列表页生成器
+    private UrlProducer urlProducer;
+    //详情页链接提取器
+    private LinkExtractor linkExtractor;
+    //详情页文本提取器
+    private TextExtractor textExtractor;
+    //初始化标志
+    private boolean isInit = false;
+
+    public void initiat(String path, UrlProducer urlProducer,LinkExtractor linkExtractor, TextExtractor textExtractor){
+        this.path = path;
+        this.urlProducer = urlProducer;
+        this.linkExtractor = linkExtractor;
+        this.textExtractor = textExtractor;
+        //初始化时将入口链接加入容器
+        urlProducer.produce(todoFrontier);
+        isInit = true;
+    }
+
+
+    //开始抓取
+    public void crawler(){
+        ExecutorService executorService = Executors.newFixedThreadPool(300);
+        while(isInit){
+            if(!todoFrontier.isEmpty()){
+                executorService.execute(new Crawler(this));
+                System.out.println("开始线程,当前线程数："+threadnum);
+            }
+        }
+    }
+}
