eb171e31f191b8e6df2eab132598c30efb14abc4
林志杰 <18814103637@163.com>
2017-03-28 09:17:29 +0800

    文本容器 将格式化文本转化为SIMHash值，存入容器，用于文本查重

---
+++ crawler/src/frontier/TextFrontier.java
@@ -0,0 +1,18 @@
+package frontier;
+
+import entity.Curl;
+
+/**
+ * Created by 林志杰 on 2017/3/27.
+ */
+public class TextFrontier {
+
+
+    public void addUrl(Curl curl){
+
+    }
+    public boolean accept(Curl curl){
+        return false;
+    }
+
+}
