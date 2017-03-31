d7a6f44967a16fbd2ed6cb982edd62c0aca39a55
林志杰 <18814103637@163.com>
2017-03-31 10:06:49 +0800

    字符工具类

---
+++ repository/src/util/StringHelper.java
@@ -0,0 +1,32 @@
+package util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by 林志杰 on 2017/3/27.
+ */
+public class StringHelper {
+    public static String replaceBlank(String str) {
+        String dest = "";
+        if (str!=null) {
+            Pattern p = Pattern.compile("\\s*|\t|\r|\n");
+            Matcher m = p.matcher(str);
+            dest = m.replaceAll("");
+        }
+        return dest;
+    }
+    public static String removeFourChar(String content) {
+        byte[] conbyte = content.getBytes();
+        for (int i = 0; i < conbyte.length; i++) {
+            if ((conbyte[i] & 0xF8) == 0xF0) {
+                for (int j = 0; j < 4; j++) {
+                    conbyte[i+j]=0x30;
+                }
+                i += 3;
+            }
+        }
+        content = new String(conbyte);
+        return content.replaceAll("0000", "");
+    }
+}
