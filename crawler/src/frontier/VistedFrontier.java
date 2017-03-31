b90d4e2640b137eb303cf9bbd222b30f2c90548a
林志杰 <18814103637@163.com>
2017-03-31 22:27:37 +0800

    visited队列 改变Berkeley数据库的初始方式

--- crawler/src/frontier/VistedFrontier.java
+++ crawler/src/frontier/VistedFrontier.java
@@ -13,48 +13,42 @@ import java.util.concurrent.TimeUnit;
  */
 public class VistedFrontier {
 
-    public VistedFrontier(String filename) {
-        this.filename = filename;
-        try{
-            //实例一个环境配置器，用于配置环境
-            EnvironmentConfig environmentConfig = new EnvironmentConfig();
-            environmentConfig.setAllowCreate(true);
-            environmentConfig.setTransactional(true);
-            environmentConfig.setReadOnly(false);
-            environmentConfig.setTxnTimeout(3000, TimeUnit.MILLISECONDS);
-
-            File file = new File(filename);
-            if(!file.exists()){
-                file.mkdirs();
-            }
-            //实例一个环境
-            visitedDbEnv = new Environment(file,environmentConfig);
-            //实例一个数据库配置对象
-            dbConfig = new DatabaseConfig();
-            dbConfig.setAllowCreate(true);
-            dbConfig.setTransactional(true);
-            dbConfig.setReadOnly(false);
-            //打开一个数据库
-            if(visitedDatabase == null){
-                visitedDatabase = visitedDbEnv.openDatabase(null,dbName,dbConfig);
-            }
-        }
-        catch(DatabaseException e){
-            e.printStackTrace();
-        }
-
-    }
-
     private String filename = "";
-    private String dbName = "";
+    private String dbname = "";
     private Environment visitedDbEnv = null;
     private DatabaseConfig dbConfig = null;
     private Database visitedDatabase = null;
 
-    public void openVisitedDatabase(){
-
+    public VistedFrontier(String filename, String dbname) {
+        this.filename = filename;
+        this.dbname = dbname;
+        //实例一个环境配置器，用于配置环境
+        EnvironmentConfig environmentConfig = new EnvironmentConfig();
+        environmentConfig.setAllowCreate(true);
+        environmentConfig.setTransactional(true);
+        environmentConfig.setReadOnly(false);
+        environmentConfig.setTxnTimeout(10000, TimeUnit.MILLISECONDS);
+        environmentConfig.setLockTimeout(10000, TimeUnit.MILLISECONDS);
+        //数据库文件
+        File file = new File(filename);
+        if(!file.exists()){
+            file.mkdirs();
+        }
+        //实例一个数据库环境
+        visitedDbEnv = new Environment(file,environmentConfig);
+        //实例一个数据库配置对象
+        dbConfig = new DatabaseConfig();
+        dbConfig.setAllowCreate(true);
+        dbConfig.setTransactional(true);
+        dbConfig.setReadOnly(false);
+        //打开一个数据库
+        if(visitedDatabase == null){
+            visitedDatabase = visitedDbEnv.openDatabase(null,dbname,dbConfig);
+        }
+        System.out.println("==============visitedDatabase数据库已打开==============");
     }
-    public synchronized void addUrl(Curl curl){
+
+    public void addUrl(Curl curl){
         try{
             DatabaseEntry ckey = new DatabaseEntry(curl.getLink().getBytes("UTF-8"));
             DatabaseEntry value = new DatabaseEntry(curl.getLink().getBytes("UTF-8"));
@@ -65,12 +59,14 @@ public class VistedFrontier {
             txn = visitedDbEnv.beginTransaction(null,txConfig);
             res = visitedDatabase.put(txn,ckey,value);
             if(res == OperationStatus.KEYEXIST){
-                System.out.println("向数据库" + dbName +"中写入:"+ckey+","+value+"失败,该值已经存在");
+                System.out.println("向数据库" + dbname +"中写入:"+ckey+","+value+"失败,该值已经存在");
             }
+            System.out.println("visited已添加链接："+curl.getLink());
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
     }
+
     public boolean accept(Curl curl){
         boolean flag = false;
         try{
@@ -84,8 +80,8 @@ public class VistedFrontier {
             if(res == OperationStatus.SUCCESS){
                 txn.commit();
             }else if(res == OperationStatus.NOTFOUND){
-                DatabaseEntry value = new DatabaseEntry(curl.getLink().getBytes("UTF-8"));
-                visitedDatabase.put(txn,ckey,value);
+                //DatabaseEntry value = new DatabaseEntry(curl.getLink().getBytes("UTF-8"));
+                //visitedDatabase.put(txn,ckey,value);
                 txn.commit();
                 flag = true;
             }
@@ -94,4 +90,16 @@ public class VistedFrontier {
         }
         return flag;
     }
+
+    public void close(){
+        if(visitedDatabase != null){
+            visitedDatabase.close();
+        }
+        if(visitedDbEnv !=  null){
+            visitedDbEnv.cleanLog();
+            visitedDbEnv.close();
+        }
+        System.out.println("==============visitedDatabase数据库已关闭==============");
+    }
+
 }
