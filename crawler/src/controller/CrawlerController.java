037429a2461a91614db3b292c95061f571f9347f
林志杰 <18814103637@163.com>
2017-03-31 22:31:57 +0800

    爬虫控制器 修改TODO队列的初始化，注入控制器实例 修改visited队列的构造，添加数据库文件的路径

--- crawler/src/controller/CrawlerController.java
+++ crawler/src/controller/CrawlerController.java
@@ -20,9 +20,9 @@ public class CrawlerController {
         return path;
     }
 
-    private VistedFrontier vistedFrontier = new VistedFrontier("visitedDB");
+    private VistedFrontier vistedFrontier = new VistedFrontier("web/resource/BerkeleyDB","visitedDB");
 
-    private TodoFrontier todoFrontier = new TodoFrontier();
+    private TodoFrontier todoFrontier = new TodoFrontier(this);
 
     public LinkExtractor getLinkExtractor() {
         return linkExtractor;
@@ -43,17 +43,17 @@ public class CrawlerController {
     //文件存储路径
     private String path = "";
     //列表页生成器
-    private UrlProducer urlProducer;
+    //private UrlProducer urlProducer = null;
     //详情页链接提取器
-    private LinkExtractor linkExtractor;
+    private LinkExtractor linkExtractor = null;
     //详情页文本提取器
-    private TextExtractor textExtractor;
+    private TextExtractor textExtractor = null;
     //初始化标志
     private boolean isInit = false;
 
     public void initiat(String path, UrlProducer urlProducer,LinkExtractor linkExtractor, TextExtractor textExtractor){
         this.path = path;
-        this.urlProducer = urlProducer;
+        //this.urlProducer = urlP;
         this.linkExtractor = linkExtractor;
         this.textExtractor = textExtractor;
         //初始化时将入口链接加入容器
@@ -64,11 +64,11 @@ public class CrawlerController {
 
     //开始抓取
     public void crawler(){
-        ExecutorService executorService = Executors.newFixedThreadPool(300);
+        System.out.println("================================开始抓取================================");
+        ExecutorService executorService = Executors.newFixedThreadPool(200);
         while(isInit){
             if(!todoFrontier.isEmpty()){
                 executorService.execute(new Crawler(this));
-                System.out.println("开始线程,当前线程数："+threadnum);
             }
         }
     }
